2edd63091eb94d2404e1d0c62afd6b765b286f5f
Sou Lee <Sou_Lee92@yahoo.com>
2021-06-14 16:43:43 -0700

    Initialize repo for leetcode/dsa

---
+++ 6_14_2021/twoSum.js
@@ -0,0 +1,12 @@
+// 1. Two Sum
+
+var twoSum = function(nums, target) {
+  let tracker = {};
+  for (let i = 0; i < nums.length; i++) {
+      let complement = target - nums[i];
+      if (tracker[complement] !== undefined) {
+          return [tracker[complement], i];
+      }
+      tracker[nums[i]] = i;
+  }
+};
