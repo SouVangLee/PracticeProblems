398660af485f552a68ecffd96a74ae927528d5fe
Sou Lee <Sou_Lee92@yahoo.com>
2021-06-14 17:34:17 -0700

    Number 9 - Palindrome Number from LeetCode

---
+++ 6_14_2021/palindromeNumber.js
@@ -0,0 +1,15 @@
+//9. Palindrome Number
+var isPalindrome = function(x) {
+  if (x < 0) return false;
+  if (x < 10) return true;
+  if (x % 10 === 0) return false;
+  
+  let reverse = 0;
+  let current = x;
+  while (current > 0) {
+      reverse *= 10;
+      reverse += (current % 10);
+      current = Math.floor(current/10);
+  }
+  return reverse === x;
+};
