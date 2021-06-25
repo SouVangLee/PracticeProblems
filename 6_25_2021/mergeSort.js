bbb9a8d86923fee9d148ac11ad13a3159adab2b4
Sou Lee <Sou_Lee92@yahoo.com>
2021-06-25 15:47:34 -0700

    Refactor mergeSort using Infinity

--- 6_25_2021/mergeSort.js
+++ 6_25_2021/mergeSort.js
@@ -1,5 +1,5 @@
 const mergeSort = function(arr) {
-  if (arr.length === 1) return arr;
+  if (arr.length <= 1) return arr;
   let midIdx = Math.floor(arr.length / 2);
   let left = mergeSort(arr.slice(0, midIdx));
   let right = mergeSort(arr.slice(midIdx));
@@ -9,14 +9,16 @@ const mergeSort = function(arr) {
 
 const merge = function(left, right) {
   let sorted = [];
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
+  while (left.length || right.length) {
+    let ele1 = left.length ? left[0] : Infinity;
+    let ele2 = right.length ? right[0] : Infinity;
+    if (ele1 < ele2) {
       sorted.push(left.shift());
     } else {
       sorted.push(right.shift())
     }
   }
-  return sorted.concat(left, right);
+  return sorted;
 }
 
-console.log(mergeSort([5,2,1,8,9,4]));
+console.log(mergeSort([5,2,1,8,9,4,7, -1]));
