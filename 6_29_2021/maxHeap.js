af885e20fe544f9c85b6bbc0977547a52e64a0be
Sou Lee <Sou_Lee92@yahoo.com>
2021-06-29 09:28:27 -0700

    Insert/Delete Heap

---
+++ 6_29_2021/maxHeap.js
@@ -0,0 +1,67 @@
+class maxHeap {
+  constructor() {
+    this.array = [null];
+  }
+
+  getParent(idx) {
+    return Math.floor(idx / 2);
+  } 
+
+  getLeftChild(idx) {
+    return 2 * idx;
+  }
+
+  getRightChild(idx) {
+    return 2 * idx + 1;
+  }
+
+  insert(val) {
+    this.array.push(val);
+    this.siftUp(this.array.length - 1);
+  }
+
+  siftUp(idx) {
+    if (idx === 1) return;
+
+    let parentIdx = this.getParent(idx);
+    if (this.array[idx] > this.array[parentIdx]) {
+      [ this.array[idx], this.array[parentIdx] ] = [ this.array[parentIdx], this.array[idx] ];
+      this.siftUp(parentIdx);
+    }
+  }
+
+  deleteMax() {
+    let max = this.array[1];
+    this.array[1] = this.array.pop();
+    this.siftDown(1); //call siftDown on the root node, which is at index 1;
+    return max;
+  }
+
+  siftDown(idx) {
+    let leftIdx = this.getLeftChild(idx);
+    let rightIdx = this.getRightChild(idx);
+    let ary = this.array;
+    let leftVal = ary[leftIdx];
+    let rightVal = ary[rightIdx];
+    let val = ary[idx];
+
+    if (leftVal === undefined) leftVal = -Infinity;
+    if (rightVal === undefined) rightVal = -Infinity;
+    if (val > leftVal && val > rightVal) return;
+
+    let swapIdx = (leftVal > rightVal) ? leftIdx : rightIdx;
+    [ ary[idx], ary[swapIdx] ] = [ ary[swapIdx], ary[idx] ];
+    this.siftDown(swapIdx);
+  }
+}
+
+let heap = new maxHeap();
+heap.insert(42);
+heap.insert(32);
+heap.insert(24);
+heap.insert(100);
+heap.insert(50);
+heap.insert(27);
+console.log(heap.array);
+console.log(heap.deleteMax());
+console.log(heap.array);
