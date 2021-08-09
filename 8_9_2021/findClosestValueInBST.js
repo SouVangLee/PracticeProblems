8f77b5f1664e48744c5c6ca1457c2b27ce663a6a
Sou Lee <Sou_Lee92@yahoo.com>
2021-08-09 15:08:43 -0700

    Find Closest Value in BST

---
+++ 8_9_2021/findClosestValueInBST.js
@@ -0,0 +1,24 @@
+//Time Complexity: O(logn) | Space Complexity: O(1);
+function findClosestValueInBst(tree, target) {
+	let minDiff = Infinity;
+	let result = Infinity;
+	let current = tree;
+	while (current) {
+		let currentDiff = Math.abs(current.value - target);
+		if (currentDiff < minDiff) {
+			minDiff = currentDiff;
+			result = current.value;
+		}
+		if (current.value > target) current = current.left;
+		else current = current.right;
+	}
+	return result;
+}
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
